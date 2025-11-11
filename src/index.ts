d2136ab7c9f50023c7133814ce3808f794ee20c3
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2025-11-11 17:35:34 +0000

    feat: moved routes to json file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
+import routesJSON from './routes.json';
+
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const url = new URL(request.url);
 
-		if (url.pathname.startsWith('/test')) {
-			return fetch('https://google.com');
+		for (const route of routesJSON.routes) {
+			console.log(`Checking route: ${route.path} -> ${route.destination}`);
+			if (url.pathname.startsWith(route.path)) {
+				console.log(`Proxying request for ${url.pathname} to ${route.destination}`);
+				return fetch(`${route.destination}${url.pathname}${url.search}`, request);
+			}
 		}
 
-		return fetch(`https://landing-page-zl2i7hciea-no.a.run.app${url.pathname}${url.search}`, request);
+		return new Response('Not Found', { status: 404 });
 	},
 } satisfies ExportedHandler<Env>;
