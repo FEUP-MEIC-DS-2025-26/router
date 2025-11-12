4a4569ad9fe8b4378751c4cd22d72e8540be09db
José Sousa <139002032+jose-carlos-sousa@users.noreply.github.com>
2025-11-12 17:48:58 +0000

    Add new route for image search

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,11 @@ export interface Route {
 
 export const routes: Route[] = [
 	{
+		path: "/image-search",
+		destination:
+			"https://frontend-821255647973.europe-west1.run.app/",
+	},
+	{
 		path: "/",
 		destination:
 			"https://landing-page-1066898156877.europe-southwest1.run.app/",
